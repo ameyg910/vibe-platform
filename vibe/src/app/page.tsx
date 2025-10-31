24d4b649ecb95d6a6f220890fe1200497f07bc53
Amey Gupta <ameygupta@Ameys-MacBook-Pro.local>
2025-10-31 16:53:45 +0530

    First

---
+++ vibe/src/app/page.tsx
@@ -0,0 +1,16 @@
+import { getQueryClient, trpc } from '@/trpc/server'; 
+import { HydrationBoundary, dehydrate } from '@tanstack/react-query';
+import { Client } from './client'; 
+import { Suspense } from 'react';
+const Page = async () => {
+   const queryClient = getQueryClient();
+   void queryClient.prefetchQuery(trpc.createAI.queryOptions({text: "Amey"}));
+   return (
+   <HydrationBoundary state={dehydrate(queryClient)}>
+      <Suspense fallback={<p>Loading...</p>}>
+      <Client />
+      </Suspense>
+   </HydrationBoundary>
+   );
+}
+export default Page;
