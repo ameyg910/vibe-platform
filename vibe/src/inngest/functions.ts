c0b1459f20eabeb0d57d53a4cc0cc59bd69dfa68
Amey Gupta <ameygupta@Ameys-MacBook-Pro.local>
2025-10-31 18:46:24 +0530

    Fourth

--- vibe/src/inngest/functions.ts
+++ vibe/src/inngest/functions.ts
@@ -2,9 +2,9 @@ import { inngest } from "./client";
 
 export const helloWorld  = inngest.createFunction(
   { id: "hello-world" },
-  { event: "test/hello-world" },
+  { event: "test/hello.world" },
     async ({ event, step }) => {
         await step.sleep("wait-1-second", "1s");
-        return { message: `Hello, ${event.data.email}!` };
+        return { message: `Hello ${event.data.email}!` };
     },
 );
